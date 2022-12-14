f58ae1d5be90d54de079f579468641ef51170542
Facuscar <facuscardillo@gmail.com>
2022-12-13 22:13:15 -0300

    Get products of each category

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -2,7 +2,11 @@ import { PrismaClient } from '@prisma/client';
 
 const handler = async (req, res) => {
   const prisma = new PrismaClient();
-  const categories = await prisma.category.findMany();
+  const categories = await prisma.category.findMany({
+    include: {
+      products: true,
+    }
+  });
 
   res.status(200).json(categories);
 }
