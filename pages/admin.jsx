0aaaf60e42b7f53b873fdda2c601a64698ba8e61
Facuscar <facuscardillo@gmail.com>
2022-12-22 19:55:32 -0300

    Fetch data with SWR and axios

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,6 +1,17 @@
+import useSWR from 'swr';
+import axios from 'axios';
 import AdminLayout from "../layout/AdminLayout";
 
 const Admin = () => {
+
+    const apiUrl = '/api/orders'
+
+    const fetcher = () => axios(apiUrl).then( data => data.data)
+
+    const { data, error, isLoading } = useSWR(apiUrl, fetcher)
+
+    console.log(data);
+
     return (
         <AdminLayout page={'Admin'}>
             <h1 className="text-4xl font-black">Admin panel</h1>
